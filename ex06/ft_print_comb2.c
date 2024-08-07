a09ee3f2efc352c7944ef3678f996610b60d7f22
Eliam Detoh <edetoh@c1r7p2.42lehavre.fr>
2024-08-07 15:20:08 +0200

    ex06 + norms

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,27 +6,57 @@
 /*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 11:30:46 by edetoh            #+#    #+#             */
-/*   Updated: 2024/08/07 11:30:48 by edetoh           ###   ########.fr       */
+/*   Updated: 2024/08/07 15:19:25 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_comb2(void)
+void	print_values(char a, char b)
 {
-	int a;
-	int b;
+	char	string[5];
 
-	for (a = 0; a != 99; a++)
+	if (a < 10)
 	{
-		for (b = a + 1; b != 99; b++)
-		{
-			char num1[1];
+		string[0] = '0' + 0;
+	}
+	else
+	{
+		string[0] = '0' + (a / 10);
+	}
+	string[1] = '0' + (a % 10);
+	string[2] = ' ';
+	if (b < 10)
+	{
+		string[3] = '0' + 0;
+	}
+	else
+	{
+		string[3] = '0' + (b / 10);
+	}
+	string[4] = '0' + (b % 10);
+	write(1, string, 5);
+}
 
-			if (a < 10)
+void	ft_print_comb2(void)
+{
+	int		a;
+	int		b;
+
+	a = 0;
+	b = a + 1;
+	while (a < 100)
+	{
+		b = a + 1;
+		while (b < 100)
+		{
+			print_values(a, b);
+			if (a != 98 || b != 99)
 			{
-				num1[0] = '0' +
+				write(1, ", ", 2);
 			}
+			b++;
 		}
+		a++;
 	}
 }
