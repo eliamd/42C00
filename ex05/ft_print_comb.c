42e9dd7dbec60e19f3e6cf7b5973914e6e1c0fe0
Eliam Detoh <edetoh@c1r7p2.42lehavre.fr>
2024-08-07 13:55:05 +0200

    ex5 norms

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,36 +6,48 @@
 /*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 11:22:32 by edetoh            #+#    #+#             */
-/*   Updated: 2024/08/07 11:30:34 by edetoh           ###   ########.fr       */
+/*   Updated: 2024/08/07 13:52:16 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	print_values(char a, char b, char c)
+{
+	char	numbers[3];
+
+	numbers[0] = '0' + a;
+	numbers[1] = '0' + b;
+	numbers[2] = '0' + c;
+	write(1, numbers, 3);
+	if (a != 7 || b != 8 || c != 9)
+	{
+		write(1, ", ", 2);
+	}
+}
+
 void	ft_print_comb(void)
 {
-	char numbers[3];
-	int a;
-	int b;
-	int c;
+	int		a;
+	int		b;
+	int		c;
 
-	for (a = 0; a <= 7; a++)
+	a = 0;
+	b = a + 1;
+	c = b + 1;
+	while (a <= 7)
 	{
-		for (b = a + 1; b <= 8; b++)
+		b = a + 1;
+		while (b <= 8)
 		{
-			for (c = b + 1; c <= 9; c++)
+			c = b + 1;
+			while (c <= 9)
 			{
-				numbers[0] = '0' + a;
-				numbers[1] = '0' + b;
-				numbers[2] = '0' + c;
-
-				write(1, numbers, 3);
-
-				if (a != 7 || b != 8 || c != 9)
-				{
-					write(1, ", ", 2);
-				}
+				print_values(a, b, c);
+				c++;
 			}
+			b++;
 		}
+		a++;
 	}
 }
